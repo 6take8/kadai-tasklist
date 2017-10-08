a71d2a435ff8926e402ca5cf2a58bbe5a46dba2a
Yuki Takei <68y.takei@gmail.com>
2017-10-08 07:29:44 +0000

    CRUD tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # トップページはタスクリスト一覧と同じページにルーティングしてくださいとの指示なので、show の方に飛ばした方がいいかも
+  root to: 'tasks#index'
+
+  resources :tasks
 end
